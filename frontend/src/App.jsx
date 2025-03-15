c22fdbf4ba1e1cd97111f026e73191297a81bc11
Bilal <bilalshahstudio@gmail.com>
2025-03-15 14:47:17 +0500

    frontend initial setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,7 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import Login from "./pages/Auth/Login";
-import Signup from "./pages/Auth/Signup";
-import Home from "./pages/Dashboard/Home";
-import Income from "./pages/Dashboard/Income";
-import Expense from "./pages/Dashboard/Expense";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Root />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/signUp" exact element={<Signup />} />
-        <Route path="/dashboard" exact element={<Home />} />
-        <Route path="/income" exact element={<Income />} />
-        <Route path="/expense" exact element={<Expense />} />
-      </Routes>
-    </Router>
-  );
+  return <div>App</div>;
 }
 
 export default App;
-
-const Root = () => {
-  //check if token exists in local storage
-  const isAuthenticated = !!localStorage.getItem("token");
-
-  //Redirected to dashboard if authenticated, otherwise to login
-  return isAuthenticated ? (
-    <Navigate to="dashboard" />
-  ) : (
-    <Navigate to="login" />
-  );
-};
--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,51 +1,7 @@
 import React from "react";
-import { Button, Divider, Form, Input } from "antd";
-import { LoginPageStyled } from "../../components/layouts/AuthLayout/authLayoutStyles";
-import Password from "antd/es/input/Password";
 
-function Login() {
-  return (
-    <LoginPageStyled>
-      <div className="login-content-container ">
-        {/* <img src={logo} className="animate__animated animate__fadeInLeft" /> */}
-        <Divider
-          type="vertical"
-          style={{ height: "300px", marginRight: "70px" }}
-        />
-        <div>
-          <h1 className="animate__animated animate__fadeIn">Login</h1>
-          <Form
-            // onFinish={handleSubmit}
-            layout="vertical"
-            initialValues={{ username: "admin", password: "admin123" }}
-            style={{ width: 300 }}
-            className="animate__animated animate__fadeInRightBig"
-          >
-            <Form.Item
-              label="Username"
-              name="username"
-              required
-              rules={[{ required: true, message: "This field is required." }]}
-            >
-              <Input placeholder="Username" type="text" />
-            </Form.Item>
-            <Form.Item label="Password" name="password" required>
-              <Password
-                style={{
-                  background: "none",
-                  borderRadius: 0,
-                }}
-                placeholder="Password"
-              />
-            </Form.Item>
-            <Button type="primary" htmlType="submit">
-              Login
-            </Button>
-          </Form>
-        </div>
-      </div>
-    </LoginPageStyled>
-  );
+function login() {
+  return <div>login</div>;
 }
 
-export default Login;
+export default login;
