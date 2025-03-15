d6a3720cffede2b917c381b279eb9cddbcc5746e
Bilal <bilalshahstudio@gmail.com>
2025-03-15 16:55:44 +0500

    removal of unnecessary npm packages

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Login = () => {
-  return <div>login</div>;
+  return (
+    <div>
+      <div>
+        <h3>Welcome Back</h3>
+        <p>Please enter your details to login</p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
