a0871a466f984a187e3b3510002afd0319dc259a
Bilal <bilalshahstudio@gmail.com>
2025-03-17 09:44:27 +0500

    ui changes

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,50 +1,101 @@
-import React from "react";
-import { Button, Divider, Form, Input } from "antd";
-import { LoginPageStyled } from "../../components/layouts/AuthLayout/authLayoutStyles";
-import Password from "antd/es/input/Password";
+import { Link, Navigate } from "react-router-dom";
+import { Button, Checkbox, Flex, Form, Input, Typography } from "antd";
 
 function Login() {
+  const validateMessages = {
+    required: "${label} is required!",
+    types: {
+      email: "${label} is not a valid email!",
+      // number: "${label} is not a valid number!",
+    },
+    // number: {
+    //   range: "${label} must be between ${min} and ${max}",
+    // },
+  };
   return (
-    <LoginPageStyled>
-      <div className="login-content-container ">
-        {/* <img src={logo} className="animate__animated animate__fadeInLeft" /> */}
-        <Divider
-          type="vertical"
-          style={{ height: "300px", marginRight: "70px" }}
-        />
-        <div>
-          <h1 className="animate__animated animate__fadeIn">Login</h1>
-          <Form
-            // onFinish={handleSubmit}
-            layout="vertical"
-            initialValues={{ username: "admin", password: "admin123" }}
-            style={{ width: 300 }}
-            className="animate__animated animate__fadeInRightBig"
+    <Flex justify="space-around" align="center">
+      <Flex vertical>
+        <Typography.Text>Budget Tracking App</Typography.Text>
+        <Flex vertical>
+          <Typography.Text>Welcome Back</Typography.Text>
+          <Typography.Text>Please enter your details to login</Typography.Text>
+        </Flex>
+        <Form
+          // name="basic"
+          name="login"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          style={
+            {
+              // maxWidth: 600,
+            }
+          }
+          initialValues={{
+            remember: true,
+            email: "admin@gmail.com",
+            password: "admin123",
+          }}
+          // onFinish={handleSubmit}
+          // onFinishFailed={onFinishFailed}
+          autoComplete="off"
+          validateMessages={validateMessages}
+        >
+          <Form.Item
+            name={["user", "email"]}
+            label="Email"
+            rules={[
+              {
+                type: "email",
+                required: true,
+              },
+            ]}
           >
-            <Form.Item
-              label="Username"
-              name="username"
-              required
-              rules={[{ required: true, message: "This field is required." }]}
+            <Input placeholder="john@example.com" />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password placeholder="Min 8 characters" />
+          </Form.Item>
+
+          <Form.Item>
+            <Flex justify="space-between" align="center">
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+              <Link to="/forgotPassword">Forgot password</Link>
+            </Flex>
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              block
+              type="primary"
+              htmlType="submit"
+              onClick={<Navigate to="dashboard" />}
             >
-              <Input placeholder="Username" type="text" />
-            </Form.Item>
-            <Form.Item label="Password" name="password" required>
-              <Password
-                style={{
-                  background: "none",
-                  borderRadius: 0,
-                }}
-                placeholder="Password"
-              />
-            </Form.Item>
-            <Button type="primary" htmlType="submit">
-              Login
+              Log in
             </Button>
-          </Form>
-        </div>
-      </div>
-    </LoginPageStyled>
+            Don't have an account? <Link to="/signup">SignUp</Link>
+          </Form.Item>
+        </Form>
+      </Flex>
+      <Flex>
+        <h1>Image to Display</h1>
+      </Flex>
+    </Flex>
   );
 }
 
