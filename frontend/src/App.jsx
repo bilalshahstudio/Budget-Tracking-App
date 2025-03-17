b8ad078a26414c094128228090ea52ef537fb991
Bilal <bilalshahstudio@gmail.com>
2025-03-17 09:44:27 +0500

    ui changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,20 +7,22 @@ import {
 } from "react-router-dom";
 import Login from "./pages/Auth/Login";
 import Signup from "./pages/Auth/Signup";
-import Home from "./pages/Dashboard/Home";
-import Income from "./pages/Dashboard/Income";
-import Expense from "./pages/Dashboard/Expense";
+import Auth from "./components/layouts/AuthLayout/AuthLayout";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import AddBudget from "./pages/AddBudget/AddBudget";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Root />} />
+        <Route path="/auth" exact element={<Auth />} />
         <Route path="/login" exact element={<Login />} />
         <Route path="/signUp" exact element={<Signup />} />
-        <Route path="/dashboard" exact element={<Home />} />
-        <Route path="/income" exact element={<Income />} />
-        <Route path="/expense" exact element={<Expense />} />
+        <Route path="/dashboard" exact element={<Dashboard />} />
+        <Route path="/home" exact element={<Home />} />
+        <Route path="/addBudget" exact element={<AddBudget />} />
       </Routes>
     </Router>
   );
@@ -30,12 +32,9 @@ export default App;
 
 const Root = () => {
   //check if token exists in local storage
-  const isAuthenticated = !!localStorage.getItem("token");
+  // const isAuthenticated = !!localStorage.getItem("token");
+  const isAuthenticated = true;
 
   //Redirected to dashboard if authenticated, otherwise to login
-  return isAuthenticated ? (
-    <Navigate to="dashboard" />
-  ) : (
-    <Navigate to="login" />
-  );
+  return isAuthenticated ? <Navigate to="dashboard" /> : <Navigate to="auth" />;
 };
--- frontend/src/components/layouts/AuthLayout/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout/AuthLayout.jsx
@@ -1,8 +1,23 @@
-import React from "react";
-import AuthLayoutStyled from "./authLayoutStyles";
+import { Layout } from "antd";
+import { Content } from "antd/es/layout/layout";
+import React, { useState } from "react";
+import Login from "../../../pages/Auth/Login";
+import Dashboard from "../../../pages/Dashboard/Dashboard";
 
-const AuthLayout = () => {
-  return <AuthLayoutStyled>AuthLayout</AuthLayoutStyled>;
-};
+function Auth() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  return (
+    <Layout>
+      <Header>Header</Header>
+      <Content>
+        {isLoggedIn ? (
+          <Dashboard isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+        ) : (
+          <Login />
+        )}
+      </Content>
+    </Layout>
+  );
+}
 
-export default AuthLayout;
+export default Auth;
