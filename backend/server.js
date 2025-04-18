baf6108dc465f49e5d3ac6585cb4ec00a4b2b73c
Bilal <bilalshahstudio@gmail.com>
2025-04-18 18:40:35 +0500

    added-timout-vercel

--- backend/server.js
+++ backend/server.js
@@ -13,17 +13,46 @@ app.use(cors());
 app.use(userRoute);
 app.use(budgetRoute);
 
-mongoose
-  .connect(process.env.URI)
-  .then(() => {
-    console.log("connected successfuly");
-    app.listen(process.env.PORT || 8000, (err) => {
-      if (err) console.log(err);
-      console.log("running at port", process.env.PORT);
-    });
-  })
-  .catch((error) => {
-    console.log("error", error);
-  });
+// Connect to MongoDB outside of handler function
+let cachedDb = null;
 
-module.exports = serverless(app);
+async function connectToDatabase() {
+  if (cachedDb) {
+    return cachedDb;
+  }
+
+  // Connect to database
+  const client = await mongoose.connect(process.env.URI);
+  cachedDb = client;
+  return cachedDb;
+}
+
+// For local development
+if (process.env.NODE_ENV !== "production") {
+  connectToDatabase()
+    .then(() => {
+      console.log("connected successfully");
+      app.listen(process.env.PORT || 8000, (err) => {
+        if (err) console.log(err);
+        console.log("running at port", process.env.PORT);
+      });
+    })
+    .catch((error) => {
+      console.log("error", error);
+    });
+}
+
+// Serverless handler
+const handler = async (event, context) => {
+  // Make sure to add this so you can re-use `cachedDb` between function calls.
+  // See https://www.mongodb.com/blog/post/serverless-development-with-nodejs-aws-lambda-mongodb-atlas
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  // Connect to database
+  await connectToDatabase();
+
+  // Pass request to express app
+  return serverless(app)(event, context);
+};
+
+module.exports = handler;
