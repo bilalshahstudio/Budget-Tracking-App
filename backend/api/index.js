79f93dc6367942c133fd6ec14c9ee18a86fc922f
Bilal <bilalshahstudio@gmail.com>
2025-04-21 13:08:24 +0500

    index file imports changes

--- backend/api/index.js
+++ backend/api/index.js
@@ -4,21 +4,24 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-const userRoute = require("../api/routes/userRoute");
-const budgetRoute = require("../api/routes/budgetRoute");
+const userRoute = require("./routes/userRoute");
+const budgetRoute = require("./routes/budgetRoute");
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+// Your routes
 app.use("/user", userRoute);
 app.use("/budget", budgetRoute);
 
-// Don’t listen to a port in serverless
+// Connect to MongoDB
 mongoose
   .connect(process.env.URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
+// Export for serverless
 module.exports = serverless(app);
