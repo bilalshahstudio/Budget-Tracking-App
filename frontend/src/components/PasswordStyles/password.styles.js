f45e2ab0a70020600a7e7e53d09c71519c686d6d
Bilal <bilalshahstudio@gmail.com>
2025-04-22 17:24:29 +0500

    Revert "Merge branch 'main' into vercel-separate"
    This reverts commit 943cc8cca505a1521baf02f7d8653ac78c17489f, reversing
    changes made to 3915d64fe675bd9db5f2758bfe39bf9eb9aeb03b.

--- frontend/src/components/PasswordStyles/password.styles.js
+++
@@ -1,7 +0,0 @@
-import Password from "antd/es/input/Password";
-import styled from "styled-components";
-
-export const PasswordStyled = styled(Password)`
-  border-radius: 0px;
-  /* background: none; */
-`;
