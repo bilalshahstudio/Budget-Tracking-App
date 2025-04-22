72288d02ae71fc651fb9319c3c51e79b5e97cb7f
Bilal <bilalshahstudio@gmail.com>
2025-04-22 19:23:33 +0500

    header console

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
+  console.log("Request headers:", req.headers);
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ message: "Access denied" });
 
